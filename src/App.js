79ab13c4aeeabfae186ec85b5a34da17459f5a52
Dominic Duke <dduke123@hotmail.com>
2022-08-15 12:32:09 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import generatePassword from './generatePassword.js';
 function App() {
+  const [characterAmount, setCharacterAmount] = useState(10);
+  const [includesUppercase, setIncludesUppercase] = useState(true);
+  const [includesNumbers, setIncludesNumbers] = useState(true);
+  const [includesSymbols, setIncludesSymbols] = useState(true);
+  const [password, setPassword] = useState('');
+  useEffect(() => {
+    
+      setPassword(
+        generatePassword(characterAmount, includesUppercase, includesNumbers, includesSymbols)
+      );
+    
+  }, [characterAmount, includesUppercase, includesNumbers, includesSymbols]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>RandoPass</h1>
       </header>
+      <section>
+        <h3 className="password-display" id="passwordDisplay">
+          {password}
+        </h3>
+        <form id="passwordGeneratorForm" className="form">
+          <label htmlFor="characterAmountNumber">Number Of Characters</label>
+          <div className="character-amount-container">
+            <input
+              type="range"
+              min="1"
+              max="50"
+              value={characterAmount}
+              id="characterAmountRange"
+              onChange={e => setCharacterAmount(e.target.value)}
+            />
+            <input
+              className="number-input"
+              type="number"
+              min="1"
+              max="50"
+              value={characterAmount}
+              onChange={e => setCharacterAmount(e.target.value)}
+              id="characterAmountNumber"
+            />
+          </div>
+
+          <label htmlFor="includeUppercase">Include Uppercase</label>
+          <input
+            type="checkbox"
+            id="includeUppercase"
+            checked={includesUppercase}
+            onChange={e => setIncludesUppercase(e.target.checked)}
+          />
+
+          <label htmlFor="includeNumbers">Include Numbers</label>
+          <input
+            type="checkbox"
+            id="includeNumbers"
+            checked={includesNumbers}
+            onChange={e => setIncludesNumbers(e.target.checked)}
+          />
+
+          <label htmlFor="includeSymbols">Include Symbols</label>
+          <input
+            type="checkbox"
+            id="includeSymbols"
+            checked={includesSymbols}
+            onChange={e => setIncludesSymbols(e.target.checked)}
+          />
+
+          <button className="btn" onClick={() => navigator.clipboard.writeText(password)}>
+            Copy to Clipboard
+          </button>
+        </form>
+      </section>
     </div>
   );
 }
